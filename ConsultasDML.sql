e96dc4a13c7c6f089e96ffcbd57bf6f95378d66f
danieelfcr <danielfcab2002@gmail.com>
2023-04-28 18:07:02 -0600

    Query statements added

--- ConsultasDML.sql
+++ ConsultasDML.sql
@@ -1,5 +1,10 @@
-select *
-from DetallePartida
+--Consultas
 
-select *
-from Partida
+--1. Reporte de cantidad de usuarios por continente
+
+--2. Cantidad de partidas por categoría para un rango de fechas dada
+
+--3. Cantidad de partidas con menos de 80 jugadores en el último trimestre--4. Ranking de jugadores por K/D por continente incluyendo únicamente a jugadores con 10 o más partidas--5. Ranking de jugadores por Win ratio por continente incluyendo únicamente a jugadores con 10 o más partidas--6. Ranking de cosméticos por su utilización efectiva--7. Listado de partidas que ha ganado un jugador indicando cantidad de kills/*8. Listado de jugadores rivales y grado de rivalidad: se define que dos jugadores son rivales si han participado 
+en una misma partida 5 o más veces y el grado de rivalidad es la cantidad de partidas que han jugado juntos*/
+
+--9. Promedio de tiempo efectivo de juego por país--10. Cantidad de partidas en las cuales ha habido al menos un jugador de Asia y uno de América y que uno de estos haya ganado la partida--11. Cantidad de partidas en las que el ganador tenga 0 kills y algún jugador tenga 10 o más kills--12. Listado de cosméticos ordenados por tipo, categoría y cantidad de partidas ganadas --13. Cantidad de partidas por tiempo de duración en minutos--14. Top 10 jugadores con más tiempo de juego--15. Ranking de jugadores según su K/D por rango de edad (13 a 15, 16 a 20, 21 a 25, 26 a 30 y mayores de 30)
